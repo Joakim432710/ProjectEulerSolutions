a73203d3988036812b8ef3a0a7391c04b7608c24
Joakim432710 <Joachim_o@spray.se>
2016-07-17 07:00:31 +0200

    Problems 25-26

---
+++ ProjectEulerSolutions/Problem25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new BigInteger(1); //F(1)
+            var b = new BigInteger(1); //F(2)
+            var c = a + b; //F(3)
+            var index = 3; 
+            while(c.ToString().Length < 1000)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+                ++index;
+            }
+            Console.WriteLine("First fib number with 1000 digits: " + c);
+            Console.WriteLine("It's index: " + index);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProjectEulerSolutions/Problem26/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Problem26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lenLongestCycle = 0;
+            var longestNumber = 0;
+
+            var number = 999;
+            while(lenLongestCycle < number)
+            {
+                var remainders = new List<int>();
+                var currentRemainder = 1; // 1 / n will always give 1 remainder for any n > 1
+                while(!remainders.Contains(currentRemainder))
+                {
+                    remainders.Add(currentRemainder);
+                    currentRemainder *= 10;
+                    currentRemainder %= number;
+                }
+                if (remainders.Count > lenLongestCycle)
+                {
+                    lenLongestCycle = remainders.Count;
+                    longestNumber = number;
+                }
+                --number;
+            }
+
+
+            Console.WriteLine("The longest cycle with length: " + lenLongestCycle);
+            Console.WriteLine("Was found for d: " + longestNumber);
+            Console.ReadLine();
+        }
+    }
+}
